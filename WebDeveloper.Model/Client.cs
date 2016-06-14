9f8b58b18d9d185677b3498b4edaf4749988eb73
evillegas83 <edgar_83_octubre@hotmail.com>
2016-06-13 22:26:19 -0500

    commit final clase 13/06/2016

---
+++ WebDeveloper.Model/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloper.Model
+{
+    public class Client
+    {
+        public int ID { get; set; }
+
+        [Display(Name ="First Name")]
+        [Required(ErrorMessage = "This first name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "This last name is required")]
+        public string LastName { get; set; }
+    }
+}
